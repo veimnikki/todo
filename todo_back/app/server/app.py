427807e3f6bb57ceebc3e55e8475631617dfabc0
Daniil Motsnyi <daniil_motsnyi@epam.com>
2021-08-29 14:27:50 +0300

    fix CORS

--- todo_back/app/server/app.py
+++ todo_back/app/server/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
 from server.routes.item import router as ItemRouter
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(ItemRouter, tags=["Item"], prefix="/item")
 
